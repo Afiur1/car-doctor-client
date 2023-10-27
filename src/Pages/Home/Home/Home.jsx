fc54ab1a2ae6d80a0bf36d5ec82c830350eccba4
Afiur1 <afiur.mgt.bu@gmail.com>
2023-10-27 22:36:54 +0200

    first commit

---
+++ src/Pages/Home/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Banner from '../Banner/Banner';
+
+const Home = () => {
+    return (
+        <div>
+
+            <Banner></Banner>
+            This is home
+        </div>
+    );
+};
+
+export default Home;
