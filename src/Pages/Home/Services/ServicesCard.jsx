53ef4aa4b35113a780cfcf9fc823f6a1aa0613c4
Afiur1 <afiur.mgt.bu@gmail.com>
2023-10-28 12:38:25 +0200

    service card updated

--- src/Pages/Home/Services/ServicesCard.jsx
+++ src/Pages/Home/Services/ServicesCard.jsx
@@ -9,9 +9,9 @@ const ServicesCard = ({service}) => {
         <figure className="px-10 pt-10">
           <img src={img} alt="Shoes" className="rounded-xl" />
         </figure>
-        <div className="card-body items-center text-center">
+        <div className="card-body items-center ">
           <h2 className="card-title">{title}</h2>
-          <p>{price}</p>
+          <p className='text-orange-500'> $Price{price}</p>
           
         </div>
       </div>
